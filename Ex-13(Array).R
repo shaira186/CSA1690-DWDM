31a82c819767c7ca6f4e386e2a2b869c6e66563f
shaira186 <113994046+shaira186@users.noreply.github.com>
2023-05-09 22:52:08 +0530

    Create Ex-13(Array).R

---
+++ Ex-13(Array).R
@@ -0,0 +1,6 @@
+#create two vectors of differeent lengths
+vector1<-c(5,9,3)
+vector2<-c(10,11,12,13,14,15)
+#take these vectors as input to the array
+result<-array(c(vector1,vector2),dim = c(3,3,2))
+print(result)
