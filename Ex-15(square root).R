9788e840fe1fe984f53912bdebaf986ec73e3d11
shaira186 <113994046+shaira186@users.noreply.github.com>
2023-05-09 22:54:12 +0530

    Create Ex-15(square root).R

---
+++ Ex-15(square root).R
@@ -0,0 +1,17 @@
+#Perfect square root
+a <- sqrt(4);
+print(paste0("sqrt(4): ", a))
+
+b <- sqrt(0);
+print(paste0("sqrt(0): ", b))
+
+#simple number
+c <- sqrt(5);
+print(paste0("sqrt(5): ", c))
+
+d <- sqrt(4.5);
+print(paste0("sqrt(4.5): ", d))
+
+#negative number
+e <- sqrt(-2);
+print(paste0("sqrt(-2): ", e))
